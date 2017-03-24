c777a46432eb6236f15357166dbec33ed480e56e
Bruce Lim <me@brucelim.com>
2017-03-23 23:40:32 -0700

    CRUD basic methods added.

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,64 @@
 'use strict';
 
+const r = require( 'rethinkdb' );
 const router = require( 'express' ).Router();
+const connect = require( '../lib/connect' );
 
 router.post( '/users', ( request, response ) => {
-    response.json( { create: 'users' } );
+    let user = Object.assign( {}, {
+        'email': request.body.email,
+        'name': request.body.name
+    } );
+
+    r.db( 'auth' ).table( 'users' )
+        .insert( user )
+        .run( request._rdb )
+        .then( cursor => cursor.toArray() )
+        .then( result => {
+            response.send( result );
+        } )
+        .catch( error => response.send( error ) );
 } );
 
 router.get( '/users', ( request, response ) => {
-    response.json( { read: 'users' } );
+    r.db( 'auth' ).table( 'users' )
+        .run( request._rdb )
+        .then( cursor => cursor.toArray() )
+        .then( result => {
+            response.send( result );
+        } )
+        .catch( error => response.send( error ) );
 } );
 
-router.put( '/users', ( request, response ) => {
-    response.json( { update: 'users' } );
+router.put( '/users/:user_id', ( request, response ) => {
+    let user_id = request.params.user_id;
+
+    r.db( 'auth' ).table( 'users' )
+        .get( user_id )
+        .update( {
+            'email': request.body.email,
+            'name': request.body.name
+        } )
+        .run( request._rdb )
+        .then( cursor => cursor.toArray() )
+        .then( result => {
+            response.send( result );
+        } )
+        .catch( error => response.send( error ) );
 } );
 
-router.delete( '/users', ( request, response ) => {
-    response.json( { delete: 'users' } );
+router.delete( '/users/:user_id', ( request, response ) => {
+    let user_id = request.params.user_id;
+
+    r.db( 'auth' ).table( 'users' )
+        .get( user_id )
+        .delete()
+        .run( request._rdb )
+        .then( cursor => cursor.toArray() )
+        .then( result => {
+            response.send( result );
+        } )
+        .catch( error => response.send( error ) );
 } );
 
 module.exports = router;
